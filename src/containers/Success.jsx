a35771e706859925a126ba102c9225ea094ee225
williamAstudillo <wsastudilloa@gmail.com>
2021-09-21 17:25:16 -0500

    [add] firebase config

--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -15,7 +15,7 @@ const Success = () => {
     <div className="Success">
       <div className="Success-content">
         <h2>{`${buyer[0].name}, Gracias por tu compra`}</h2>
-        <span>Tu pedido lelgara en 3 dias a tu direccion:</span>
+        <span>Tu pedido lelgara en 3 dias a tu direccion::</span>
         <div className="Success-map">
           <Map data={location} />
         </div>
