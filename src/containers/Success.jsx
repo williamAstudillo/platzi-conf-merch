00b2a41213c8403cc71307e2fabdd65cadf827db
williamAstudillo <wsastudilloa@gmail.com>
2021-09-21 18:38:45 -0500

    [add] firebase config

--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -5,8 +5,8 @@ import useGoogleAddress from '../hooks/useGoogleAddress';
 import '../styles/components/Success.css';
 
 const Success = () => {
-  const { state } = useContext(AppContext);
-  const { buyer } = state;
+  // const { state } = useContext(AppContext);
+  // const { buyer } = state;
   const location = useGoogleAddress('bogota');
   // const location = useGoogleAddress(buyer[0].address);
 
@@ -14,7 +14,7 @@ const Success = () => {
   return (
     <div className="Success">
       <div className="Success-content">
-        <h2>{`${buyer[0].name}, Gracias por tu compra`}</h2>
+        {/* <h2>{`${buyer[0].name}, Gracias por tu compra`}</h2> */}
         <span>Tu pedido lelgara en 3 dias a tu direccion::</span>
         <div className="Success-map">
           <Map data={location} />
--- src/hooks/useGoogleAddress.js
+++ src/hooks/useGoogleAddress.js
@@ -1,14 +1,13 @@
 import { useState, useEffect } from 'react';
-import env from 'react-dotenv';
 import axios from 'axios';
 
+const { API_GOOGLE } = process.env;
 const useGoogleAddress = (address) => {
   const [map, setMap] = useState();
-  const API = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${env.API_GOOGLE}`;
+  const API = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${API_GOOGLE}`;
   
   useEffect(async () => {
     const response = await axios(API);
-    console.log(env.API_GOOGLE, 'responseeeeeeeeeeee');
     setMap(response.data.results[0].geometry.location);
   }, []);
   return map;
